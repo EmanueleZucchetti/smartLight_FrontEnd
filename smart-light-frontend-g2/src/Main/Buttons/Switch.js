4203ccc6bc7f6f71ac834c3d7a14c50adb16290d
angeloChecked <angi99552@gmail.com>
2018-12-13 15:22:10 +0100

    Merge branch 'master' of /Users/angelo/Documents/GitHub/smartLight_FrontEnd with conflicts.

--- smart-light-frontend-g2/src/Main/Buttons/Switch.js
+++ smart-light-frontend-g2/src/Main/Buttons/Switch.js
@@ -2,38 +2,29 @@ import React, { Component } from "react";
 import Switch from "react-switch";
 import SwitchManager from "../Service/SwitchManager";
 
-class SwitchButton extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isChecked: null
-        }
+class SwitchButton extends Component {
+    constructor() {
+        super();
+        this.state = { checked: false };
+        this.handleChange = this.handleChange.bind(this);
     }
 
-
-
-componentWillMount() {
-        this.setState({isChecked: this.props.isChecked});
+    handleChange(checked) {
+        this.setState({ checked });
     }
-
 
     render() {
-
         return (
-            <div className="switch-container">
-                <label>
-                    <input ref="switch" checked={this.state.isChecked} onChange={this._handleChange} className="switch"
-                           type="checkbox"/>
-                    <div>
-
-                        <div></div>
-                    </div>
-                </label>
-            </div>
+            <label htmlFor="normal-switch">
+                <span>Switch with default style</span>
+                <Switch
+                    onChange={this.handleChange}
+                    checked={this.state.checked}
+                    id="normal-switch"
+                />
+            </label>
         );
     }
 }
 
-    export default SwitchButton;
+export default SwitchButton;
